f0300a78d80c12d4991d3c27e33dcf1dfe61816e
Frédéric VIRICEL <99799464+xxxPOUPOUxxx@users.noreply.github.com>
2025-10-19 21:46:32 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/testlauncher/main.go
@@ -0,0 +1,23 @@
+// cmd/testlauncher/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "testlauncher/internal/testlauncher"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := testlauncher.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
